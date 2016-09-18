28722b623e7d68b69632a39b760b5758768207b2
pengjialing <pengjialing@pset.suntec.net>
2016-09-18 20:32:15 +0800

    modify

--- spider.py
+++ spider.py
@@ -7,19 +7,32 @@
 import urllib2
 import sys
 import re
+import sqlite3
 from bs4 import BeautifulSoup
  
 def taobao(url):
     page = urllib2.urlopen(url)
     html_doc = page.read()
-    soup = BeautifulSoup(html_doc.decode('utf8','ignore'))
+    soup = BeautifulSoup(html_doc.decode('utf-8','ignore'))
+    cx = sqlite3.connect("/home/pengjialing/git/test.db")
+    cu = cx.cursor()
+    cu.execute('drop table shangpin')
+    cu.execute('create table if not exists shangpin (id integer primary key, nid varchar(20), name varchar(50), price decimal(18,2))')
     for i in soup.find_all('script'):
         string = i.get_text()
-        one = re.findall(r'"nid":"\d+"', string)
-        if one:
-        	for j in one:
-        		nid = j.split("\"")[3]
-        		print nid
+        namelist = re.findall(r'"raw_title":"[\s\S]*"', string)
+        nidlist = re.findall(r'"nid":"\d+"', string)
+        pricelist = re.findall(r'"view_price":"\d+\.?\d+"', string)
+        if nidlist:
+        	for j in range(len(nidlist)):
+                        name = namelist[j].splite("\"")[3]
+        		nid = nidlist[j].split("\"")[3]
+                        price = pricelist[j].split("\"")[3]
+                        cu.execute("insert into shangpin (nid, name, price) values (?, ?, ?)", (nid,name,price))
+    cx.commit()
+    cx.close()
+
+
  
 keyword = sys.argv[1]
 taobao("https://s.taobao.com/search?q="+keyword)
