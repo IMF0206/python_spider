832b525b51c021e6d5267cd72f25c209e72c7198
pengjialing <pengjialing@pset.suntec.net>
2016-09-21 20:42:15 +0800

    aaa

--- jingdong.py
+++ jingdong.py
@@ -10,17 +10,18 @@ import re
 import sqlite3
 from bs4 import BeautifulSoup
  
-def jingdong(url,keyword):
-    page = urllib2.urlopen(url)
-    html_doc = page.read()
-    soup = BeautifulSoup(html_doc.decode('utf-8','ignore'))
-    print soup
-    cx = sqlite3.connect("/home/pengjialing/git/test.db")
-    cu = cx.cursor()
-    cu.execute('drop table if exists '+keyword)
-    cu.execute('create table if not exists '+keyword+' (id integer primary key, nid varchar(20), name varchar(50), price decimal(18,2))')
-    for i in soup.find_all('script'):
-        string = i.get_text()
+def jingdong(url):
+    # page = urllib2.urlopen(url)
+    # html_doc = page.read()
+    html_doc = open(r'/home/pengjialing/git/python_spider/jd.html')
+    soup = BeautifulSoup(html_doc)
+    print soup.prettify()
+    # cx = sqlite3.connect("/home/pengjialing/git/test.db")
+    # cu = cx.cursor()
+    # cu.execute('drop table if exists '+keyword)
+    # cu.execute('create table if not exists '+keyword+' (id integer primary key, nid varchar(20), name varchar(50), price decimal(18,2))')
+    # for i in soup.find_all('script'):
+        # string = i.get_text()
         # print string
         # namelist = re.findall(ur'"raw_title":"[^\"]+"', string)
         # nidlist = re.findall(r'"nid":"\d+"', string)
@@ -31,10 +32,11 @@ def jingdong(url,keyword):
         #         nid = nidlist[j].split("\"")[3]
         #                 price = pricelist[j].split("\"")[3]
         #                 cu.execute('insert into '+keyword+' (nid, name, price) values (?, ?, ?)', (nid,name,price))
-    cx.commit()
-    cx.close()
+    # cx.commit()
+    # cx.close()
 
 
  
-keyword = sys.argv[1]
-jingdong("http://search.jd.com/Search?keyword="+keyword+"&enc=utf-8",keyword)
+# keyword = sys.argv[1]
+# jingdong("http://search.jd.com/Search?keyword="+keyword+"&enc=utf-8")
+jingdong("aaa")
--- spider.py
+++ spider.py
@@ -10,29 +10,31 @@ import re
 import sqlite3
 from bs4 import BeautifulSoup
  
-def taobao(url,keyword):
-    page = urllib2.urlopen(url)
-    html_doc = page.read()
-    soup = BeautifulSoup(html_doc.decode('utf-8','ignore'))
+def taobao(keyword):
     cx = sqlite3.connect("/home/pengjialing/git/test.db")
     cu = cx.cursor()
     cu.execute('drop table if exists '+keyword)
     cu.execute('create table if not exists '+keyword+' (id integer primary key, nid varchar(20), name varchar(50), price decimal(18,2))')
-    for i in soup.find_all('script'):
-        string = i.get_text()
-        namelist = re.findall(ur'"raw_title":"[^\"]+"', string)
-        nidlist = re.findall(r'"nid":"\d+"', string)
-        pricelist = re.findall(r'"view_price":"\d+\.?\d+"', string)
-        if nidlist:
-        	for j in range(len(nidlist)):
-                        name = namelist[j].split("\"")[3]
-        		nid = nidlist[j].split("\"")[3]
-                        price = pricelist[j].split("\"")[3]
-                        cu.execute('insert into '+keyword+' (nid, name, price) values (?, ?, ?)', (nid,name,price))
+    for i in range(5):
+        url = "https://s.taobao.com/search?q="+keyword+"&s="+str(44*i)
+        page = urllib2.urlopen(url)
+        html_doc = page.read()
+        soup = BeautifulSoup(html_doc.decode('utf-8','ignore'))
+        for i in soup.find_all('script'):
+            string = i.get_text()
+            namelist = re.findall(ur'"raw_title":"[^\"]+"', string)
+            nidlist = re.findall(r'"nid":"\d+"', string)
+            pricelist = re.findall(r'"view_price":"\d+\.?\d+"', string)
+            if nidlist:
+        	    for j in range(len(nidlist)):
+                            name = namelist[j].split("\"")[3]
+        		    nid = nidlist[j].split("\"")[3]
+                            price = pricelist[j].split("\"")[3]
+                            cu.execute('insert into '+keyword+' (nid, name, price) values (?, ?, ?)', (nid,name,price))
     cx.commit()
     cx.close()
 
 
  
 keyword = sys.argv[1]
-taobao("https://s.taobao.com/search?q="+keyword,keyword)
+taobao(keyword)
